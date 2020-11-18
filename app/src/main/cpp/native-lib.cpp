d6aa213cac632cdc58ee7becbc3d3f41008223f1
haojiangbo <2100272728@qq.com>
2020-11-18 22:40:10 +0800

    安卓JNI技术整合

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,16 +1,59 @@
 #include <jni.h>
 #include <string>
 #include "stdio.h"
+#include <android/log.h>
+
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include <libavutil/pixfmt.h>
+#include <libavutil/opt.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/samplefmt.h>
+#include "libavutil/imgutils.h"
+#include <libswresample/swresample.h>
+#include <libavdevice/avdevice.h>
+}
+#define LOG_TAG "HJB>>>>>"
+#define ALOGV(...) ((void)__android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__))
+#define ALOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__))
+#define ALOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__))
+#define ALOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__))
+#define ALOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__))
+
+
+void show_audio_device() {
+    AVFormatContext *formatContext = avformat_alloc_context();
+    AVDictionary *options = NULL;
+    av_dict_set(&options, "list_devices", "true", 0);
+    AVInputFormat *iformat = av_find_input_format("vfw");
+    ALOGE("Device Info=============\n");
+    avformat_open_input(&formatContext, "video=dummy", iformat, &options);
+    ALOGE("========================\n");
+}
+
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_haojiangbo_ndkdemo_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    printf("test c++ \n");
-    fflush(stdout);
+    ////////////////// 源设置//////////////////////
+    AVFormatContext *formatContext;
+    // 注册所有编码器
+    av_register_all();
+    // 支持网络
+    avformat_network_init();
+    // 得到格式化工厂的上下文
+    formatContext = avformat_alloc_context();
+    show_audio_device();
+
     return env->NewStringUTF(hello.c_str());
 }
+
+
+
 
 
 
@@ -20,3 +63,106 @@ Java_com_haojiangbo_ndkdemo_MainActivity_message(JNIEnv *env, jobject thiz) {
     // TODO: implement message()
 
 }
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_haojiangbo_ndkdemo_MainActivity_startMp3(JNIEnv *env, jobject thiz, jstring url) {
+    char *filePath = (char *) env->GetStringUTFChars(url, NULL);
+    ALOGE("filePath == %s", filePath);
+    AVFormatContext *formatContext;
+    // 注册所有编码器
+    av_register_all();
+    // 支持网络
+    avformat_network_init();
+    // 得到格式化工厂的上下文
+    formatContext = avformat_alloc_context();
+    //开始准备解复用
+    int r = avformat_open_input(&formatContext,filePath,NULL,NULL);
+    if(r != 0){
+        ALOGE("avformat_open_input open error %d \n",r);
+        return ;
+    }
+    // 转储格式信息
+    av_dump_format(formatContext, -1, NULL, 0);
+    // 查找流
+    r = avformat_find_stream_info(formatContext,NULL);
+    if(r < 0){
+        ALOGE("avformat_find_stream_info not find stream \n");
+        return ;
+    }
+    // 找到音频流下标
+    int audioIndex = -1;
+    for(int i = 0; i < formatContext->nb_streams; i++){
+        if(formatContext->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO){
+            audioIndex = i;
+            break;
+        }
+    }
+    if(audioIndex == -1){
+        ALOGE("audio stream index not defind \n");
+        return;
+    }
+    AVCodecContext *audioCodecContext = formatContext->streams[audioIndex]->codec;
+    AVCodec *audioCodec = avcodec_find_decoder(audioCodecContext->codec_id);
+    ALOGE("采样率  %d \n",audioCodecContext->sample_rate);
+    ALOGE("通道  %d \n",audioCodecContext->channels);
+    // 打开音频解码器
+    r = avcodec_open2(audioCodecContext,audioCodec,NULL);
+    if(r != 0){
+        ALOGE("avcodec_open2 is error\n");
+        return;
+    }
+    AVFrame *catchFrame = av_frame_alloc();
+    int i = 0;
+    //重采样
+    SwrContext *swr = swr_alloc_set_opts(NULL,  // we're allocating a new context
+                                         audioCodecContext->channel_layout,  // out_ch_layout
+                                         AV_SAMPLE_FMT_S16,    // out_sample_fmt
+                                         audioCodecContext->sample_rate,    // out_sample_rate
+                                         audioCodecContext->channel_layout, // in_ch_layout
+                                         audioCodecContext->sample_fmt,   // in_sample_fmt
+                                         audioCodecContext->sample_rate,   // in_sample_rate
+                                         0,                    // log_offset
+                                         NULL);                // log_ctx
+    // 初始化
+    swr_init(swr);
+    const int bufSize = av_samples_get_buffer_size(NULL, 1,1152, AV_SAMPLE_FMT_S16, 1);
+    uint8_t* data1 = (uint8_t*)malloc(bufSize);
+    uint8_t* data2 = (uint8_t*)malloc(bufSize);
+    memset(data1, 0, bufSize);
+    memset(data2, 0, bufSize);
+    uint8_t *audio_data_buffer[2] = { data1, data2 };
+    AVPacket avPacket;
+    while (av_read_frame(formatContext,&avPacket) >= 0){
+        if(avPacket.stream_index == audioIndex){
+            int isOk = 0;
+            // 开始解码
+            int r =  avcodec_decode_audio4(audioCodecContext,catchFrame,&isOk,&avPacket);
+            if(r < 0){
+                continue;
+            }
+            int isPlanar =  av_sample_fmt_is_planar(audioCodecContext->sample_fmt);
+            if(isOk){
+                i++;
+                int  ret = swr_convert(swr, audio_data_buffer, catchFrame->nb_samples, (const uint8_t **) catchFrame->extended_data, catchFrame->nb_samples);
+                ALOGE("swr_convert >>>> %d ", ret);
+
+                //通过反射调用java中的方法
+                //找class 使用FindClass方法，参数就是要调用的函数的类的完全限定名，但是需要把点换成/
+                jclass clazz = env->FindClass("com/haojiangbo/ndkdemo/MainActivity");
+                //获取对应的函数: 参数1:类class,参数2:方法名,参数3:方法签名
+                //ps:方法签名的获取:进入build->intermediates->classes->debug目录下,使用javap -s 类的完全限定名,就能获得函数签名
+                jmethodID method = env->GetMethodID(clazz, "printRet", "(I)V");
+                //实例化该class对应的实例  使用AllocObject方法，使用clazz创建该class的实例。
+                //调用方法
+                env->CallVoidMethod(thiz, method,ret);
+            }
+        }
+    }
+    av_frame_free(&catchFrame);
+    avcodec_close(audioCodecContext);
+    avformat_close_input(&formatContext);
+}
+
+
--- app/src/main/java/com/haojiangbo/ndkdemo/MainActivity.java
+++ app/src/main/java/com/haojiangbo/ndkdemo/MainActivity.java
@@ -1,9 +1,22 @@
 package com.haojiangbo.ndkdemo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
 import android.widget.TextView;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 /**
  * 安卓 ffmpeg 编译
@@ -29,11 +42,31 @@ import android.widget.TextView;
  * 11
  * https://www.jianshu.com/p/0a7f3175c1b9
  *
+ *
+ * C++ 与 java 方法的互相调用
+ * https://blog.csdn.net/xfhy_/article/details/82801706
+ *
+ * 方法签名
+ * https://blog.csdn.net/u011305680/article/details/75000349
+ *
+ *
+ * 关于安卓工具 每次都需要 download maven metadata 的问题
+ * 一般都是 gradle 的依赖 有不确定的依赖版本 版本后面带 + 号的
+ * 去掉就可以了
  */
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
+
+        // avutil avresample swresample swscale avcodec avformat avfilter avdevice
+        System.loadLibrary("avutil");
+        System.loadLibrary("avresample");
+        System.loadLibrary("swresample");
+        System.loadLibrary("swscale");
+        System.loadLibrary("avcodec");
+        System.loadLibrary("avformat");
+        System.loadLibrary("avdevice");
         System.loadLibrary("native-lib");
     }
 
@@ -45,7 +78,19 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        if(!initPermission()){
+           return;
+        }
+
+        this.startMp3(Environment.getExternalStorageDirectory().getAbsolutePath()+"/mp3/test.mp3");
     }
+
+
+    public void printRet(int t){
+        Log.e("HJB>>>>>","call back"+t);
+    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
@@ -55,4 +100,93 @@ public class MainActivity extends AppCompatActivity {
 
 
     public native String message();
+
+
+    public native void startMp3(String url);
+
+
+    //////////////////////////////////////动态申请权限模块///////////////////////////////////////
+
+    String [] quanxianList = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO
+    };
+    /* Manifest.permission.CAMERA,
+    Manifest.permission.RECORD_AUDIO,
+     Manifest.permission.ACCESS_FINE_LOCATION,
+     Manifest.permission. READ_PHONE_STATE,*/
+    private final int mRequestCode = 100;//权限请求码
+    private boolean  initPermission() {
+        int sta = 0;
+        for(String s : quanxianList){
+            if (ContextCompat.checkSelfPermission(this, s) != PERMISSION_GRANTED) {
+                sta++;
+            }
+        }
+        if(sta > 0){
+            ActivityCompat.requestPermissions(this, quanxianList, mRequestCode);
+        }
+        return sta > 0 ? false : true;
+    }
+
+    //请求权限后回调的方法
+    //参数： requestCode  是我们自己定义的权限请求码
+    //参数： permissions  是我们请求的权限名称数组
+    //参数： grantResults 是我们在弹出页面后是否允许权限的标识数组，数组的长度对应的是权限名称数组的长度，数组的数据0表示允许权限，-1表示我们点击了禁止权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean hasPermissionDismiss = false;//有权限没有通过
+        if (mRequestCode == requestCode) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] == -1) {
+                    hasPermissionDismiss = true;
+                }
+            }
+            //如果有权限没有被允许
+            if (hasPermissionDismiss) {
+                showPermissionDialog();/// 跳转到系统设置权限页面，或者直接关闭页面，不让他继续访问
+                //this.finish();
+            }
+        }
+    }
+
+    /**
+     * 不再提示权限时的展示对话框
+     */
+    AlertDialog mPermissionDialog;
+    private static final int NOT_NOTICE = 2;//如果勾选了不再询问
+    private void showPermissionDialog() {
+        if (mPermissionDialog == null) {
+            mPermissionDialog = new AlertDialog.Builder(this)
+                    .setMessage("已禁用权限，请手动授予")
+                    .setPositiveButton("设置", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            cancelPermissionDialog();
+                            Uri packageURI = Uri.parse("package:" +  getPackageName());
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
+                            startActivityForResult(intent, NOT_NOTICE);
+                        }
+                    })
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //关闭页面或者做其他操作
+                            cancelPermissionDialog();
+                            MainActivity.this.finish();
+                        }
+                    })
+                    .create();
+        }
+        mPermissionDialog.show();
+    }
+    //关闭对话框
+    private void cancelPermissionDialog() {
+        mPermissionDialog.cancel();
+    }
+
 }
