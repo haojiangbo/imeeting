fc8cc8d6aa25520a442b9f6202026070a810b2ea
haojiangbo <2100272728@qq.com>
2020-11-24 09:34:45 +0800

    无用代码删除

--- multimedia/src/main/java/com/haojiangbo/hander/udp/UdpServerMessageReadHander.java
+++ multimedia/src/main/java/com/haojiangbo/hander/udp/UdpServerMessageReadHander.java
@@ -18,28 +18,10 @@ import java.net.InetSocketAddress;
 
 @Slf4j
 public class UdpServerMessageReadHander extends ChannelInboundHandlerAdapter {
-    File file = new File("D:/video/udp.mp3");
-    int totalByte = 0;
-    OutputStream outputStream;
-   /* public UdpServerMessageReadHander(){
-        if(file.exists()){
-            file.delete();
-        }
-        try {
-            outputStream = new FileOutputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }*/
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         DatagramPacket datagramPacket = (DatagramPacket) msg;
-       /* StringBuilder stringBuilder = new StringBuilder();
-        ByteBufUtil.appendPrettyHexDump(stringBuilder, datagramPacket.content());
-        log.info(">>> ip >>> {} \n  {}",
-                datagramPacket.sender(),
-                stringBuilder.toString());*/
         int totalSize = datagramPacket.content().readableBytes();
         MediaDataProtocol protocol = MediaDataProtocol.byteBufToMediaDataProtocol(datagramPacket.content());
         ReferenceCountUtil.release(msg);
@@ -64,18 +46,6 @@ public class UdpServerMessageReadHander extends ChannelInboundHandlerAdapter {
                 break;
         }
 
-
-        //ctx.writeAndFlush(new DatagramPacket(datagramPacket.content(),datagramPacket.sender()));
-        //ctx.writeAndFlush(new DatagramPacket(datagramPacket.content(),new InetSocketAddress("192.168.43.208", 10089)));
-        //ctx.writeAndFlush(new DatagramPacket(datagramPacket.content(), new InetSocketAddress("10.10.10.234", 10089)));
-        /*try {
-            byte [] b = new byte[datagramPacket.content().readableBytes()];
-            datagramPacket.content().readBytes(b);
-            outputStream.write(b);
-            outputStream.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
     }
 
     private void mediaProtocolForword(ChannelHandlerContext ctx, DatagramPacket datagramPacket, int totalSize, MediaDataProtocol protocol, String number) {
